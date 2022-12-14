dc0140904450361fd4926b9f4c4b8b68fec0befb
PaulKimaniNjuguna <paulkimaninj@gmail.com>
2022-12-14 23:50:52 +0300

    first

---
+++ routes/web.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/master', function () {
+    return view('layouts.master');
+});
+
+Route::get('/about', function(){
+    return view('layouts.about');
+});
+
+Route::get('/home', function(){
+    return view('layouts.home');
+});
+
+Route::get('/feature', function(){
+    return view('layouts.feature');
+});
+
+Route::get('/service', function(){
+    return view('layouts.service');
+});
+
+Route::get('/product', function(){
+    return view('layouts.product');
+});
+
+Route::get('/testimonial', function(){
+    return view('layouts.testimonial');
+});
+
+Route::get('/contact', function(){
+    return view('layouts.contact');
+});
+
+Route::get('/team', function(){
+    return view('layouts.team');
+});
+
+Route::get('/gallery', function(){
+    return view('layouts.gallery');
+});
